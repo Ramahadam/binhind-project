90adb4e4410f2a8532e1f51fd30346418100da7e
Mohamed <ramy.adam33@gmail.com>
2025-08-17 15:04:15 +0400

    feat: build navination menu

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,24 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { DM_Sans } from 'next/font/google';
+import './globals.css';
+import Navbar from './_components/Navbar';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const dmSans = DM_Sans({
+  variable: '--font-dm-sans',
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  display: 'swap',
 });
 
 export const metadata = {
-  title: "Binhind-homepage",
-  description: "Binhind consultant",
+  title: 'Binhind-homepage',
+  description: 'Binhind consultant',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${dmSans.variable}  antialiased`}>
+        <Navbar />
         {children}
       </body>
     </html>
